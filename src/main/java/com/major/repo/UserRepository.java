0404517f967269139ffcb4a7fffd25018d3ab15a
Yusuf Ali <yusufali2205@gmail.com>
2016-03-12 01:52:05 -0600

    adding user repositories

---
+++ src/main/java/com/major/repo/UserRepository.java
@@ -0,0 +1,16 @@
+package com.major.repo;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.major.model.User;
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+	List<User> findByLastName(@Param("name") String name);
+
+}
